8c1618f7109070eac657850ddb8bf1d88c67ff76
Gregoire Le Bras <gregoire.le.bras@gmail.com>
2022-02-17 18:42:20 +0000

    test

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -5,7 +5,6 @@ with tripdata as
   select *,
     row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
   from {{ source('staging','fhv_tripdata') }}
-  where dispatching_base_num is not null 
 )
 select
     -- identifiers
@@ -21,7 +20,7 @@ select
     
 
 from tripdata
-where rn = 1
+--where rn = 1
 
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
